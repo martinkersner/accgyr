035ce9251014131185d5b7c665420f4cc97f5251
Martin <m.kersner@gmail.com>
2016-07-09 22:51:53 +0900

    display smoothed X, Y and Z accelerations

--- exploreData.R
+++ exploreData.R
@@ -1,16 +1,27 @@
-#csv_file <- 'acc_forward2.csv'
-#csv_file <- 'acc_sitandup1.csv'
-#csv_file <- 'acc_chairforward2.csv'
-#csv_file <- 'acc_stairsdown2.csv'
-#csv_file <- 'acc_stairsup1.csv'
-csv_file <- 'prep_data/acc_leftrightfast2.csv'
+#csv_file <- 'prep_data/acc_forward1.csv'
+#csv_file <- 'prep_data/acc_forward2.csv'
+#csv_file <- 'prep_data/acc_sitandup1.csv'
+#csv_file <- 'prep_data/acc_sitandup2.csv'
+#csv_file <- 'prep_data/acc_stairsdown1.csv'
+csv_file <- 'prep_data/acc_stairsdown2.csv'
+#csv_file <- 'prep_data/acc_chairforward1.csv'
+#csv_file <- 'prep_data/acc_chairforward2.csv'
+
+# PARAMETERS
+window<-50 # data contains about 50 measurements per second
+ylim <- c(-5,16)
 
 data <- read.csv(csv_file)
 head(data)
 
-#id <- data$ID
-#id <- data[, c("ID")]
-#acc <- data[, c("X", "Y", "Z")]
-#plot(data$ID, data$X, type="o", col="blue")
+#X <- data$X
+#Y <- data$Y
+#Z <- data$Z
 
-boxplot(data[, c("X", "Y", "Z")])
+X <- rollmean(data$X, k=window, fill=c(0,0,0))
+Y <- rollmean(data$Y, k=window, fill=c(0,0,0))
+Z <- rollmean(data$Z, k=window, fill=c(0,0,0))
+
+plot(data$ID, Z, type="o", col="blue", ylim=ylim)
+lines(data$ID, X, type="o", col="red")
+lines(data$ID, Y, type="o", col="green")
